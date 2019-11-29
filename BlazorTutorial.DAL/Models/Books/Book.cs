6b9898593831e9aa19d13e91c71aa3ee25b8fe9c
Nathan <natemi1010@gmail.com>
2019-11-29 18:21:49 +0000

    End Of Video 4 - Components

---
+++ BlazorTutorial.DAL/Models/Books/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorTutorial.DAL.Models.Books
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Overview { get; set; }
+        public int Price { get; set; }
+    }
+}
--- BlazorTutorial/Startup.cs
+++ BlazorTutorial/Startup.cs
@@ -1,4 +1,4 @@
-using BlazorTutorial.Data;
+using BlazorTutorial.Core.Services.Books;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -15,17 +15,14 @@ namespace BlazorTutorial
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+      
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            services.AddScoped<IBookService, BookService>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -35,7 +32,6 @@ namespace BlazorTutorial
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
