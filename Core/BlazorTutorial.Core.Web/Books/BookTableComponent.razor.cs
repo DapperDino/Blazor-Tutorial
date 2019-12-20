d67587966697afe5e1745b07762896621f78e69d
Nathan <natemi1010@gmail.com>
2019-12-20 19:21:56 +0000

    End Of Video 6 - Table Overview

---
+++ Core/BlazorTutorial.Core.Web/Books/BookTableComponent.razor.cs
@@ -0,0 +1,12 @@
+﻿using BlazorTutorial.DAL.Models.Books;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace BlazorTutorial.Core.Web.Books
+{
+    public partial class BookTableComponent : ComponentBase
+    {
+        [Parameter]
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
---
+++ Web/BlazorTutorial.Web/Pages/BooksPage.razor.cs
@@ -0,0 +1,19 @@
+﻿using BlazorTutorial.Core.Services.Books;
+using BlazorTutorial.DAL.Models.Books;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorTutorial.Web.Pages
+{
+    public partial class BooksPage : ComponentBase
+    {
+        [Inject]
+        public IBookService BookService { get; set; }
+
+        private Book[] books;
+
+        protected override void OnInitialized()
+        {
+            books = BookService.GetBooks();
+        }
+    }
+}
